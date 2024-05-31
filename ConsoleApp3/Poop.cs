514c4d17eba1a71424ae66d2c39505552bcf5480
qwwweq <qweqwe@wqeqe>
2024-05-31 09:37:32 +0300

    urd

---
+++ ConsoleApp3/Poop.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp3;
+
+public class Poop
+{
+    private List<object> _objects = new List<object>();
+
+    public void AddObjectToPoop(object item)
+    {
+        _objects.Add(item);
+    }
+
+    public void ClearPoop()
+    {
+        _objects = new List<object>();
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp3;
+Console.WriteLine("Hello, World!");
+
+Poop trash = new Poop();
+
+trash.AddObjectToPoop("табуретка");
+trash.AddObjectToPoop("табуsad");
+trash.ClearPoop();
+trash.AddObjectToPoop("");
+
